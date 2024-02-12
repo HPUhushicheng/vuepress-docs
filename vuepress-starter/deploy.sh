1800ad455048f944f8b900ffc0756c867c8298f6
hushicheng <120257354+HPUhushicheng@users.noreply.github.com>
2024-02-12 10:18:28 +0800

    Update deploy.sh

--- vuepress-starter/deploy.sh
+++ vuepress-starter/deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:HPUhushicheng/vuepress-blog.git master:gh-pages
+git push -f git@github.com:HPUhushicheng/vuepress-docs.git master:gh-pages
 
 cd -
